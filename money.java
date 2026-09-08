5387feb92e1cfc4d8c7ac2de0d1826e018b89f92
SirSiri <chiossigustavo@gmail.com>
2026-09-08 16:10:36 -0300

    commit etapa 4

--- money.java
+++ money.java
@@ -1,33 +1,36 @@
 abstract class Money {
-    protected int amount;
-    public boolean equals(Object object) {
-        Money money = (Money) object;
-        return amount == money.amount
-                && getClass().equals(money.getClass());
-    }
-    static Dollar dollar(int amount) {
-        return new Dollar(amount);
-    }
-    static Franc franc(int amount) {
-        return new Franc(amount);
-    }
-    abstract Money times(int multiplier);
+   private String currency; 
+
+   static Money dollar(int amount)  {
+      return new Dollar(amount, "USD");
+   }
+
+   static Money franc(int amount) {
+      return new Franc(amount, "CHF");
+   }
+
+   Money(int amount, String currency) {
+      this.amount = amount;
+      this.currency = currency;
+   }
 }
 
-class Dollar extends Money {
-    Dollar(int amount) {
-        this.amount = amount;
-    }
-    Money times(int multiplier) {
-        return new Dollar(amount * multiplier);
-    }
+class Franc extends Money {	
+   Franc(int amount, String currency) {
+      super(amount, currency);
+   }
+     
+   Money times(int multiplier)  {
+      return Money.franc(amount * multiplier);
+   }
 }
 
-class Franc extends Money {
-    Franc(int amount) {
-        this.amount = amount;
-    }
-    Money times(int multiplier) {
-        return new Franc(amount * multiplier);
-    }
+class Dollar extends Money {	
+   Dollar(int amount, String currency)  {
+      super(amount, currency);
+   }
+	
+   Money times(int multiplier)  {
+      return Money.dollar(amount * multiplier);
+   }
 }
